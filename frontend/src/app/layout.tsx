e6cceb536fce468ab2bf934f40f612ab0fbd16b8
Mikhail <mikhailbelov737@gmail.com>
2025-10-29 00:02:38 +0100

    feature: setup Theme Provider & created toggle mode button

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,9 +1,10 @@
+import { ThemeProvider } from "@/providers/ThemeProvider";
 import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Messenger",
-  description: "Generate by create next appd",
+  // description: "Generate by create next app",
 };
 
 export default function RootLayout({
@@ -12,8 +13,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
