17f5ed4300c9f3abf63de9ae3f7bcb291147293c
Mikhail <mikhailbelov737@gmail.com>
2026-01-12 19:29:18 +0100

    feat: create protected routes

---
+++ frontend/src/shared/components/ProtectedRoute.tsx
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+import { Navigate } from "react-router-dom";
+import { useTokenStore } from "@/stores/tokenStore";
+
+interface ProtectedRouteProps {
+  children: React.ReactNode;
+}
+
+export const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
+  const refreshToken = useTokenStore((state) => state.refreshToken);
+
+  if (!refreshToken) {
+    return <Navigate to="/register" replace />;
+  }
+
+  return <>{children}</>;
+};
