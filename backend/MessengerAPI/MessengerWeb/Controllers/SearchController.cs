d4d20ef3022ae9f72e149677b80949a7872c2dc4
danil <danilovmv06@gmail.com>
2026-01-12 23:24:56 +0100

    searchextend

---
+++ backend/MessengerAPI/MessengerWeb/Controllers/SearchController.cs
@@ -0,0 +1,43 @@
+﻿using Messenger.Application.Dtos;
+using Messenger.Application.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MessengerWeb.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("[controller]")]
+public class SearchController : ControllerBase
+{
+    private readonly UserService _userService;
+    
+    public SearchController(UserService userSer)
+    {
+        _userService = userSer;
+    }
+    
+    [HttpGet("by-name")]
+    public async Task<IActionResult> SearchByName([FromQuery] string query)
+    {
+        if (string.IsNullOrWhiteSpace(query))
+            return BadRequest("Search query is required");
+        
+        var users = await _userService.SearchUsersByNameSubstring(query);
+        
+        if (users == null || users.Count == 0)
+            return NotFound("No users found");
+        
+        var userDtos = users.Select(u => new UserDto
+        {
+            Id = u.Id,
+            Username = u.Username,
+            Name = u.Name,
+            PhoneNumber = u.PhoneNumber,
+            AvatarUrl = u.AvatarUrl,
+            Status = u.Status
+        }).ToList();
+        
+        return Ok(userDtos);
+    }
+}
