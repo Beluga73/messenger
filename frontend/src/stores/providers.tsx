b3bf027178ba63246301e2c3be1a93669ebd9df3
Mikhail <mikhailbelov737@gmail.com>
2026-02-01 11:14:44 +0100

    feat: add launchDarkly

--- frontend/src/stores/providers.tsx
+++ frontend/src/stores/providers.tsx
@@ -2,22 +2,29 @@ import { useEffect } from "react";
 import type { ReactNode } from "react";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { asyncWithLDProvider } from "launchdarkly-react-client-sdk";
 
 import { Toaster } from "@/shared/components/ui/sonner";
-import { useThemeStore } from "@/stores/themeStore";
 
-export const RootProviders = ({ children }: { children: ReactNode }) => {
+// import { useThemeStore } from "@/stores/themeStore";
+
+export const RootProviders = async ({ children }: { children: ReactNode }) => {
   const queryClient = new QueryClient();
-  const initTheme = useThemeStore((state) => state.initTheme);
+  const LDProvider = await asyncWithLDProvider({
+    clientSideID: import.meta.env.VITE_LAUNCH_DARKLY_CLIENT_SIDE_ID,
+  });
+  // const initTheme = useThemeStore((state) => state.initTheme);
 
-  useEffect(() => {
-    initTheme();
-  }, [initTheme]);
+  // useEffect(() => {
+  //   initTheme();
+  // }, [initTheme]);
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <Toaster position="top-center" richColors />
-      {children}
-    </QueryClientProvider>
+    <LDProvider>
+      <QueryClientProvider client={queryClient}>
+        <Toaster position="top-center" richColors />
+        {children}
+      </QueryClientProvider>
+    </LDProvider>
   );
 };
